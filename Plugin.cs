0ea6a3d2b66ef479832cb89c66e570a7fec4a0b3
Robyn <rnixon98@gmail.com>
2024-07-26 23:59:04 -0500

    initial commit

---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace LethalCompanyModding.Template;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public static ManualLogSource Log;
+
+    private void Awake()
+    {
+        /*
+            BepinEx makes you a ManualLogSource for free called "Logger"
+            and I created a static value above to hold on to it so other
+            parts of your plugin's code can find it by using Plugin.Log
+
+            We assign it here
+        */
+        Log = Logger;
+
+        // Log our awake here so we can see it in LogOutput.txt file
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} is loaded!");
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Host Furniture Reset
+A simple mod for Lethal Company that resets all unlocked furniture that is not in storage. Useful if your terminal disappeared!
+
+> [!NOTE]
+> Will not run on clients so its safe to distribute in a mod pack but please do not!
+
+# Use
+Simply use your favorite mod manager to add this mod to your profile then load your saved game and it will reset all furniture. Then exit the game to save it and remove this mod
+
+> [!WARNING]
+> If you do not remove this mod from your modlist it will reset your furniture every time you load a save!
