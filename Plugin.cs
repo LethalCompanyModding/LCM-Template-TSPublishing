aa30bc6e5f96b931d06addf49d02bdf07d1777c5
Damon Blais <damon@rethunk.tech>
2024-07-29 19:25:50 -0700

    style: Standardize indentation and formatting across project files

--- Plugin.cs
+++ Plugin.cs
@@ -2,32 +2,32 @@
 using BepInEx.Logging;
 
 /*
-    Here are some basic resources on code style and naming conventions to help
-    you in your first CSharp plugin!
+  Here are some basic resources on code style and naming conventions to help
+  you in your first CSharp plugin!
 
-    https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
-    https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/identifier-names
-    https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/names-of-namespaces
+  https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
+  https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/identifier-names
+  https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/names-of-namespaces
 */
 
 [BepInPlugin(LCMPluginInfo.PLUGIN_GUID, LCMPluginInfo.PLUGIN_NAME, LCMPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
-    public static ManualLogSource Log = null!;
+  public static ManualLogSource Log = null!;
 
-    private void Awake()
-    {
-        /*
-            BepinEx makes you a ManualLogSource for free called "Logger"
-            and I created a static value above to hold on to it so other
-            parts of your plugin's code can find it by using Plugin.Log
+  private void Awake()
+  {
+    /*
+      BepinEx makes you a ManualLogSource for free called "Logger"
+      and I created a static value above to hold on to it so other
+      parts of your plugin's code can find it by using Plugin.Log
 
-            We assign it here
-        */
-        Log = Logger;
+      We assign it here
+    */
+    Log = Logger;
 
-        // Log our awake here so we can see it in LogOutput.txt file
-        Log.LogInfo($"Plugin {LCMPluginInfo.PLUGIN_NAME} is loaded!");
-    }
+    // Log our awake here so we can see it in LogOutput.txt file
+    Log.LogInfo($"Plugin {LCMPluginInfo.PLUGIN_NAME} is loaded!");
+  }
 
 }
